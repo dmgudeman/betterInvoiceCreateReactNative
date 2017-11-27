f72cf7cca48e7ea12259d2bb014cf5ab5dc5487f
davidmgudeman <davidmgudeman@gmail.com>
2017-11-27 09:21:04 -0800

    fixed the companyEditScreen so that it updates appropriately

--- screens/CompanyEditScreen/CompanyEditScreen.js
+++ screens/CompanyEditScreen/CompanyEditScreen.js
@@ -7,7 +7,7 @@ import {
   FormInput, 
   FormValidationMessage, 
 }                               from 'react-native-elements';
-import { connect } from 'react-redux';
+import { connect, connectAdvanced } from 'react-redux';
 import * as actions from '../../actions';
 
 class CompanyEditScreen extends Component {
@@ -16,7 +16,12 @@ class CompanyEditScreen extends Component {
     console.log('COMPANYEDITSCREEN COMPONENTWILLMOUNT this.props', this.props);
     let navigator = this.props.navigation.state.params;
     const {address, color, companyKey, fUserId, hourly, id, name, paymentTerms } = navigator.company
-    this.props.companyUpdate('company', navigator.company);
+    // this.props.companyUpdate('company', navigator.company);
+    this.props.companyUpdate('name', name);
+    this.props.companyUpdate('paymentTerms', paymentTerms);
+    this.props.companyUpdate('color', color);
+    this.props.companyUpdate('hourly', hourly);
+    this.props.companyUpdate('address', address);
     console.log('COMPANYEDITSCREEN COMPONENETWILLMOUNT props object', {address, color, fUserId, hourly,id, name, paymentTerms }  );
   }
   onSubmit(props, companyEdit) {
@@ -37,6 +42,11 @@ class CompanyEditScreen extends Component {
   
   render() {
     console.log('CompanyEditScreen render this.props', this.props);
+    if (this.props.company === undefined) console.log('RENDER THIS.PROPS.COMPANY IS UNDEFINED');
+    if (this.props.company){
+    console.log('CompanyEditScreen render this.props.company', this.props.company);
+    console.log('CompanyEditScreen render this.props.company.name', this.props.company.name);
+    }
     return (
       <View>
          <FormLabel>Name</FormLabel>
@@ -87,26 +97,26 @@ class CompanyEditScreen extends Component {
  
 
 const mapStateToProps = (state) => {
-  console.log('COMPANYEDIT MAPSTATETOPROPS state.companies.company', state.companies.company);
-  console.log('xxxxxxxxxxxxxxxxx', state.companies.company === undefined  );
+  console.log('COMPANYEDIT MAPSTATETOPROPS state.companies', state.companies);
+  console.log('state.companies is undefined ', state.companies === undefined  );
   console.log(state);
-  if (state.companies.company) {
-    // console.log('COMPANYEDIT MAPSTATETOPROPS state.companies.company.active', state.companies.company.active);
+  if (state.companies) {
+    // console.log('COMPANYEDIT MAPSTATETOPROPS state.companies.active', state.companies.active);
     const paymentTermsOptionsList = [{option: ''}, {option: "30"}, {option: "15"}, {option: "5"}] ;
     const colorOptionsList = [{option:'blue', option:'green'},{option:'yellow'}, {option: 'purple'},{option: 'brown'},{option: 'red'}]
-    const active = state.companies.company.active || true;
+    const active = state.companies.active || true;
 
-    const companyKey = state.companies.company.companyKey || '';
-    const address = state.companies.company.address || '';
+    const companyKey = state.companiesKey || '';
+    const address = state.companies.address || '';
     const location = state.location || null;
 
-    const color = state.companies.company.color || 'blue';
+    const color = state.companies.color || 'blue';
     const fUserId = state.auth.fUserId || '';
-    const hourly = state.companies.company.hourly || '';
-    const name = state.companies.company.name || '';
-    const paymentTerms = state.companies.company.paymentTerms || '30';
+    const hourly = state.companies.hourly || '';
+    const name = state.companies.name || '';
+    const paymentTerms = state.companies.paymentTerms || '30';
     const userId = state.auth.userId || '';
-    console.log('COMPANYEDITSCREEN MAPSTATETOPROPS props', paymentTermsOptionsList, colorOptionsList, 
+    console.log('COMPANYEDITSCREEN MAPSTATETOPROPS props OBJECT', paymentTermsOptionsList, colorOptionsList, 
     companyKey, active, address, location, color, 
     fUserId, hourly, name, paymentTerms, userId);
     
