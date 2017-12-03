2c345bc78e517acfcae56aa18a397f144d632a58
davidmgudeman <davidmgudeman@gmail.com>
2017-12-03 09:08:14 -0800

    sorting code into separate functions on the onSubmit InvoiceCreateScreen, it was spahgetti.

--- screens/InvoiceCreateScreen.js
+++ screens/InvoiceCreateScreen.js
@@ -4,7 +4,8 @@ import { bindActionCreators }   from 'redux';
 import { 
   View, 
   Text, 
-  DatePickerIOS 
+  // DatePickerIOS 
+  Alert,
 }                               from 'react-native';
 import { connect }              from 'react-redux';
 import thunk from 'redux-thunk';
@@ -29,10 +30,14 @@ class InvoiceCreateScreen extends Component {
     this.props.invoiceCreateClear({ companyKey, coItems, coName, paymentTerms, fUserId });
   }
   calcDueDate(date){
-    let a = moment(date);
+    
+    console.log('INVOICECREATESCREEN CALCDUEDATE this.props.createdAt', this.props.createdAt); 
+    let a = moment(this.props.createdAt);
     a.add(this.props.paymentTerms, 'days');
-    this.dueDate = a.format(); 
-    return this.dueDate;
+    let dueDate = a._i;
+    console.log('INVOICECREATESCREEN CALCDUEDATE a._i', a._i); 
+    console.log('INVOICECREATESCREEN CALCDUEDATE a', a); 
+    this.props.invoiceUpdate('dueDate', dueDate);
   }
   
   filterByDateRange(beginDate, endDate, coItems) {
@@ -40,64 +45,74 @@ class InvoiceCreateScreen extends Component {
     let bmDate = moment(beginDate).format();
     let emDate = moment(endDate).format();
     let filteredItems = [];
-   
-
-    if(coItems){
-      let itemsArray = (Object).values(coItems);
+    let itemsArray = (Object).values(filteredItems);
       // console.log('INVOICECREATE FILTERBYDATERANGE this.props.items', this.props.items);
       // console.log('INVOICECREATE FILTERBYDATERANGE itemsArray', itemsArray);
-      itemsArray.forEach(i => {
-        imDate = moment(i.date);
-        if (imDate.isSameOrAfter(bmDate, 'day') && imDate.isSameOrBefore(emDate, 'day')) {
-          filteredItems.push(i);
-        
-        }
-        console.log('INVOICECREATESCREEN FILTERBYDATERANGE filteredItems', filteredItems);
-        this.props.invoiceUpdate('items', filteredItems)
-      })
-    }
-    // console.log('INVOICECREATE FILTERBYDATERANGE', filteredItems);
-    if(filteredItems.length>0) return filteredItems;
-    return 0;
-  } 
-  onSubmit = async () => {
-    const {  beginDate, companyKey, coItems, coName, createdAt, description, discount, dueDate, endDate, fUserId, invoiceKey, items, total} = this.props
-    console.log('111111111111111InvoicecreateScreen onSubmit this.props.coItems', this.props.coItems);
-    
-
-    let  filteredItems = await this.filterByDateRange(beginDate, endDate, coItems);
+    itemsArray.forEach(i => {
+      imDate = moment(i.date);
+      if (imDate.isSameOrAfter(bmDate, 'day') && imDate.isSameOrBefore(emDate, 'day')) {
+        filteredItems.push(i);
+      }
+      console.log('INVOICECREATESCREEN FILTERBYDATERANGE filteredItems', filteredItems);
+    })
+    if (!filteredItems) this.props.invoiceUpdate('items', "0")
     this.props.invoiceUpdate('items', filteredItems);
-    console.log('INVOICECREATE ONSUBMIT AFTER filterItems this.props.items', this.props.items );
-    newDueDate = await this.calcDueDate(this.createdAt);
-    this.props.invoiceUpdate('dueDate', newDueDate);
-    // console.log('dueDate', this.props.dueDate);
-    // console.log('22222222222222222InvoicecreateScreen onSubmit this.props', this.props);
+  }
+  filteredItemsAlert(){
+    if(!this.props.items) {
+      Alert.alert(
+        'Invoice Items',
+        'There are no invoice items for this date range'
+        ,[{text: 'Cancel', onPress: () => this.props.navigation.navigate('companies')}]
+      )
 
-    if (this.props.items){
+    } else {
       let invoiceTotal = 0;
       // console.log('this.props.items', this.props.items);
       let itemsArray = (Object).values(this.props.items);
-      console.log('itemsArray ', itemsArray);
-        itemsArray.forEach(i => {
-          console.log('iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiinvoiceTotal', invoiceTotal);
-          invoiceTotal = invoiceTotal + i.total;
+      // console.log('itemsArray ', itemsArray);
+      itemsArray.forEach(i => {
+        // console.log('iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiinvoiceTotal', invoiceTotal);
+        invoiceTotal = invoiceTotal + i.total;
         });
       // console.log('invoiceTotal', invoiceTotal);
-      if (invoiceTotal) {
-        this.props.invoiceUpdate('total', invoiceTotal);
-
-      } else {
-        this.props.invoiceUpdate('total', 0);
-      }
+      this.props.invoiceUpdate('total', invoiceTotal);
     }
-    // console.log('INVOICECREATE ONSUBMIT THIS.PROPS ',  this.props);
-    
+  }
+
+  calcInvoiceTotal =  () => {
+    if (this.props.items){
+      let invoiceTotal = 0;
+      let itemsArray = (Object).values(this.props.items);
+        itemsArray.forEach(i => {
+          invoiceTotal = invoiceTotal + i.total;
+      });
+      // if (invoiceTotal) {
+      //   this.props.invoiceUpdate('total', invoiceTotal);
+
+      // } else {
+      //   this.props.invoiceUpdate('total', 0);
+      // }
+    }
+  }
+  onSubmit = async () => {
+    const {  beginDate, companyKey, coItems, coName, createdAt, description, discount, dueDate, endDate, fUserId, invoiceKey, items, total} = this.props
+    console.log('CREATEINVOICESCREEN onSubmit createdAt,', createdAt, );
+    console.log('CREATEINVOICESCREEN onSubmit beginDate', beginDate);
+    console.log('CREATEINVOICESCREEN onSubmit  endDate', endDate);
+    await this.filterByDateRange(beginDate, endDate, coItems);
+    await this.filteredItemsAlert();
+    await this.calcDueDate(createdAt);
+    await this.calcInvoiceTotal();
+   
   
-    let invoice = {beginDate, companyKey, coName, createdAt,  
+    let invoice = {
+      beginDate, companyKey, coName, createdAt,  
       description: this.props.description, discount, 
       dueDate: this.props.dueDate, endDate, fUserId,
-      invoiceKey, items: this.props.items, total: this.props.total}
-    // console.log('INVOICECREATE ONSUBMIT THIS.PROPS  ------ after ', this.props);
+      invoiceKey, items: this.props.items, total: this.props.total
+    }
+    
     this.props.invoiceCreate({invoice})
     await this.props.navigation.goBack();
   }
