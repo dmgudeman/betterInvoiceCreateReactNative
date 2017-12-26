6130a6fd9c880b1bd3968742ae40895334c066b4
davidmgudeman <davidmgudeman@gmail.com>
2017-12-26 10:29:08 -0800

    continued editting WVContainer

--- screens/WVContainer.js
+++ screens/WVContainer.js
@@ -4,7 +4,6 @@ import { WebView, TouchableHighlight, Text, View, StyleSheet, Dimensions } from 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import _ from 'lodash';
 import * as actions from '../actions';
-import axios from 'axios';
 
 // const webapp = require('../assets/MyWebView2/index.html');
 const webapp = require('../assets/InvoicesPrePDF.html')
@@ -19,6 +18,11 @@ class WVContainer extends Component {
   }
 
   componentWillMount() {
+    // let navigator = this.props.navigation.state.params;
+    
+    // // console.log('WVContainer COMPONENTWILLMOUNT navigator.description', navigator.description);
+    // // console.log('WVContainer COMPONENTWILLMOUNT navigator', navigator);
+    // const invoice = navigator.invoice;
     // this.props.companyUpdate('invoice', invoice);
     // console.log('WVContainer COMPONENTWILLMOUNT this.props', this.props);
   }
@@ -40,7 +44,7 @@ class WVContainer extends Component {
   }
 
   sendPostMessage() {
-    console.log( 'REACTNATIVE sendPostMessage JSON.stringify(this.props.invoice)', JSON.stringify(this.props.invoice) );
+    console.log( 'REACTNATIVE sendPostMessage this.props.invoice', JSON.stringify(this.props.invoice) );
     let x = JSON.stringify(this.props.invoice)
     this.webView.postMessage(x);
   }
@@ -49,48 +53,52 @@ class WVContainer extends Component {
   //   this.webView.postMessage (`${this.props.description}` );
   //   this.webView.invoice =  this.props.invoice
   // }
-  sendWVtoCloud= () => {
-      axios.post ('https://us-central1-better-invoice-firebase.cloudfunctions.net/pdfMaker',
-      <WebView
-        ref={webView => {this.webView = webView; }}
-        onMessage={() =>this.onMessage}
-        source={ webapp}
-        style={styles.webview}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        scalesPageToFit={true}
-        onLoad={()=>{
-          this.sendPostMessage();
-          invoice = payload
-          }
-        }
-        />)
-        .then((response)=>{
-          console.log('WebView Sent ');
-          console.log('response', response);
-
-        })
-        .catch((error)=> {
-          console.log('ERRRORRR', error);
-        })
-  }
 
   render() {
+    // this.webview.postMessage("Hello from RN");
+
     const payload = this.props.invoice;
-    const itemsArray = _.map(payload.items, (val, id) => { return { ...val, id}; });
-    payload.itemsArray = itemsArray;
-    console.log('WVContainer render payload', payload);
+    const itemsArray = _.map(payload.items, (val, id) => {
+      
+        return { ...val, id};
+      });
+      payload.itemsArray = itemsArray;
+      console.log('WVContainer render payload', payload);
     return (
+
       <View style={{flex:1, alignItems: 'flex-end'}}> 
-         <Button
-          title= "Make PDF"
-          onPress ={this.sendWVtoCloud} 
-          backgroundColor={ 
-            this.state.controls.hours.valid 
-            ?'#bdc3c7':'#bdc3c745'}
-        /> 
-      </View>
+        <WebView
+          // source={{uri: 'https://github.com/facebook/react-native'}}
+          ref={webView => {this.webView = webView; }}
+          onMessage={() =>this.onMessage}
+          source={ webapp}
+          style={styles.webview}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          scalesPageToFit={true}
+          onLoad={()=>{
+            this.sendPostMessage();
+            // this.sendPostMessage2();
+            invoice = payload
+          }
+          
+            }
+          
+        //   injectedJavaScript={`()=> {
+        //       console.log('HI THERE')
+        //       var p = document.getElementById('description');
+        //       var q = document.getElementById('name')
+        //       p.firstChild.nodeValue = "Description: "${this.props.description};
+        //       q.firstChild.nodeValue = "Name: "${this.props.name};
+        //     }
+        //   `
+        // }
+        />
+        {/* <TouchableHighlight style={{padding: 10, backgroundColor: 'blue', marginTop: 20}} onPress={() => this.sendPostMessage()}>
+          <Text style={{color: 'white', fontSize: 45}}>Send post message from react native</Text>
+        </TouchableHighlight> */}
+       </View>
     );
   }
 }
