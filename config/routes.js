e219b9acbbf58a0ddd39b878235cdb30e62588f6
davidmgudeman <davidmgudeman@gmail.com>
2017-12-27 08:44:21 -0800

    continuing to refactor with a test webview

--- config/routes.js
+++ config/routes.js
@@ -17,7 +17,7 @@ import InvoicesScreen from '../screens/InvoicesScreen';
 import ListItem from '../components/ListItem';
 import MyDatePicker from '../components/MyDatePicker';
 import WVContainer from '../screens/WVContainer';
-import MyWebView from '../components/MyWebView.html'; 
+import MyWebView from '../components/MyWebView'; 
 
 
 
