4d3fc8b73c6138ac889f52f1db5febd6b17b07c1
davidmgudeman <davidmgudeman@gmail.com>
2017-12-28 12:51:54 -0800

    ditto

--- screens/CompaniesScreen.js
+++ screens/CompaniesScreen.js
@@ -20,17 +20,19 @@ class CompaniesScreen extends Component {
       headerRight:
         <Button
           title="+Business"
+          // onPress= {_.debounce(()=>navigation.navigate('companyCreate'), 2000,{'leading':true, 'trailing':false})}
           onPress={async () => {
             await console.log('COMPANIES NAV OPTIONS navigation', navigation);
             await navigation.state.params.clearCompany();
             await navigation.navigate('companyCreate');
             }
           }
-        />,
-      headerLeft: null,
-    };
-  }
+        />
+      ,
+      headerLeft: null
+    }
 
+  }
 
   componentWillMount() {
     console.log('COMPANIES CWM this.props.navigation.state.key', this.props.navigation.state.key);
@@ -73,46 +75,49 @@ class CompaniesScreen extends Component {
   };
   renderItem = ({ item, index }) => {
     // console.log('COMPANIES RENDERITEM  item', item );
-    return (
-      <ListItem
-        company={item}
-        navigation={this.props.navigation}
-        goToInvoices={this.goToInvoices}
-        goToInvoiceCreate={this.goToInvoiceCreate}
-        goToItems={this.goToItems}
-        goToItemCreate={this.goToItemCreate}
-        goToCompanyEdit={this.goToCompanyEdit}
+    return (<
+        ListItem company={item}
+                 navigation={this.props.navigation}
+                 goToInvoices={this.goToInvoices}
+                 goToInvoiceCreate={this.goToInvoiceCreate}
+                 goToItems={this.goToItems}
+                 goToItemCreate={this.goToItemCreate}
+                 goToCompanyEdit={this.goToCompanyEdit}
       />
     )
   }
 
   render() {
     const navigation = this.props.navigation
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.props.companies}
-          renderItem={this.renderItem}
-          keyExtractor={(item) => item.id}
+    return (<
+      View style={styles.container}>
+      <
+        FlatList data={this.props.companies}
+                 renderItem={this.renderItem}
+                 keyExtractor={
+                   (item) => item.id
+                 }
 
-        />
-      </View>
-    );
-  }
-}
+      />
+      <
+      /
+      View>
+      );
+      }
+      }
 
-const mapStateToProps = (state) => {
-  const fUserId = state.auth.fUserId || '';
-  const company = state.company || '';
-  const companies = _.map(state.companies, (val, id) => {
-    return { ...val, id };
-  });
+      const mapStateToProps = (state) => {
+      const fUserId = state.auth.fUserId || '';
+      const company = state.company || '';
+      const companies = _.map(state.companies, (val, id) => {
+      return {...val, id};
+    });
 
-  return {
-    fUserId,
-    companies,
-    company,
-  };
-};
+      return {
+      fUserId,
+      companies,
+      company,
+    };
+    };
 
-export default connect(mapStateToProps, actions)(CompaniesScreen);
+      export default connect(mapStateToProps, actions)(CompaniesScreen);
